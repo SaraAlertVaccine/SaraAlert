475929f2b52d08d7143c9e010b7feec3a70a0e4f
Justin Kufro <jkufro@mitre.org>
2020-12-01 15:36:58 -0500

    remove commented out code

--- app/controllers/dosages_controller.rb
+++ app/controllers/dosages_controller.rb
@@ -7,19 +7,6 @@ class DosagesController < ApplicationController
   def create
     redirect_to root_url && return unless current_user.can_create_patient_dosages?
     dosage = Dosage.new(dosage_params)
-    # dosage = Dosage.new(
-    #   cvx: params.permit(:cvx)[:cvx],
-    #   manufacturer: params.permit(:manufacturer)[:manufacturer],
-    #   expiration_date: params.permit(:expiration_date)[:expiration_date],
-    #   lot_number: params.permit(:lot_number)[:lot_number],
-
-    #   date_given: params.permit(:date_given)[:date_given],
-    #   sending_org: params.permit(:sending_org)[:sending_org],
-    #   admin_route: params.permit(:admin_route)[:admin_route],
-    #   admin_suffix: params.permit(:admin_suffix)[:admin_suffix],
-    #   admin_site: params.permit(:admin_site)[:admin_site],
-    #   dose_number: params.permit(:dose_number)[:dose_number]
-    # )
     dosage.patient_id = params.permit(:patient_id)[:patient_id]
     dosage.save
     History.vaccine_dosage(
@@ -33,18 +20,6 @@ class DosagesController < ApplicationController
     redirect_to root_url && return unless current_user.can_edit_patient_dosages?
     dosage = Dosage.find_by(id: params.permit(:id)[:id])
     dosage.update(dosage_params)
-    # dosage.update(
-    #   cvx: params.permit(:cvx)[:cvx],
-    #   manufacturer: params.permit(:manufacturer)[:manufacturer],
-    #   expiration_date: params.permit(:expiration_date)[:expiration_date],
-    #   lot_number: params.permit(:lot_number)[:lot_number],
-
-    #   date_given: params.permit(:date_given)[:date_given],
-    #   sending_org: params.permit(:sending_org)[:sending_org],
-    #   admin_route: params.permit(:admin_route)[:admin_route],
-    #   admin_suffix: params.permit(:admin_suffix)[:admin_suffix],
-    #   admin_site: params.permit(:admin_site)[:admin_site],
-    #   dose_number: params.permit(:dose_number)[:dose_number])
     dosage.save
     History.vaccine_dosage_edit(patient: params.permit(:patient_id)[:patient_id],
                             created_by: current_user.email,
